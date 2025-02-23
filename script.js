0bdd7016457786bb6700b06452b53347160439ae
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-02-23 15:33:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -273,6 +273,11 @@ var queries = getQueryParameter('queries');
 const ur2 = "https://example.com" + ( (queries) ? '?' + atob(queries) : '');
 window.queryParam = getQueryParams(ur2);
 
+if(fill == "screen"){
+   document.querySelector('.top-nav').style.display = 'none';
+   document.querySelector('.side-nav').style.display = 'none';
+}
+
 if (page) {
     loadPage(page + '.html');
 } else {

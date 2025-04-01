2f9d6287574d594c95a76a77a104cd3669730b6e
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-01 09:42:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -290,24 +290,8 @@ function loadPage(pageUrl , queries , fills) {
         const doc = parser.parseFromString(html, 'text/html'); 
         try{
             doc.querySelectorAll(".design img").forEach(img => img.removeAttribute("src"));
-            const observer3 = new MutationObserver((mutations) => {
-              mutations.forEach((mutation) => {
-                if (mutation.type === 'childList' && mutation.target.innerHTML.trim() !== '') {
-                  console.log('Elements have been added to:', mutation.target);
-                  mutation.target.parentNode.querySelectorAll(".design").forEach((elem) => {
-                    elem.remove();
-                  });
-                }
-              });
-            });
-        
-            doc.querySelectorAll(".final").forEach((element) => {
-              observer3.observe(element, {
-                childList: true,
-                subtree: true
-              });
-            });
-        }catch(err){ console.error(err); }
+            doc.querySelectorAll(".design").forEach(elem => elem.remove()); 
+        }catch{}
         const links = doc.querySelectorAll('link');
         const styles = doc.querySelectorAll('style');
         const scripts = doc.querySelectorAll('script');
